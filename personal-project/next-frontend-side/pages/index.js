9bd291a6b2f508a923d6fb45774d631f070f547d
michellegaletti <msgaletti@gmail.com>
2021-06-14 11:57:20 -0600

    Adding custom icon to app

--- personal-project/next-frontend-side/pages/index.js
+++ personal-project/next-frontend-side/pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <link rel="tabicon" href="../public/favicon.ico" />
       </Head>
       <PageLayout />
     </div>
